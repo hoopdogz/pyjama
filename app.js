570572bb01be6aa417b5cfee6cb0c5bf4608c6d0
ben <bgregor04@gmail.com>
2020-03-06 13:57:18 -0500

    add args functionality

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 const port = process.env.PORT || 4000
 
 const public_path = path.join(__dirname, './public')
-const db_path = path.join(__dirname, './db')
+const db_path = path.join(__dirname, './py')
 app.use(express.static(public_path))
 
 app.get('', (req, res) => {
@@ -17,7 +17,21 @@ app.get('', (req, res) => {
 app.get('/py/:name', (req, res) => {
   try {
     var dataToSend
-    const python = spawn('python', [`${db_path}/${req.params.name}.py`])
+
+    let python
+    console.log(req.query.params)
+
+    if (req.query.params) {
+      // python = spawn('python', [`${db_path}/${req.params.name}.py`,`${req.query.params}`])
+      let args = [`${db_path}/${req.params.name}.py`]
+      let param_split = (req.query.params).split(" ")
+      args = args.concat(param_split)
+      console.log(args)
+      python = spawn('python', args)
+    } else {
+      python = spawn('python', [`${db_path}/${req.params.name}.py`])
+    }
+
     python.stdout.on('data', function (data) {
       console.log(`Run ${req.params.name}.py`)
       dataToSend = data.toString()
---
+++ py/test_args.py
@@ -0,0 +1,4 @@
+import sys
+
+print("You gave me", len(sys.argv), "arguments. Thanks.")
+print ("They were:", str(sys.argv))
